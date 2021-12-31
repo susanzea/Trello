5abe987b1548569ca0a56e470e89d298c9301060
Susan Zea <susanzea16@gmail.com>
2021-12-31 14:07:31 -0500

    user controller created with create action

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::UsersController < ApplicationController
+    def create
+        @user = User.new(new_user_params)
+
+        if @user.save
+            login(user)
+            render 'api/users/show'
+        else
+            render json: ['Invalid Signup Credentials'], status: 422
+        end
+    end
+
+    def new_user_params
+        params.require(:user).permit(:username, :email, :full_name, :password)
+    end
+end
