9d965855a3775f709a96a1ec0012b9dfbc1e90af
Susan Zea <susanzea16@gmail.com>
2021-12-31 14:01:40 -0500

    user model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,48 @@
 class User < ApplicationRecord
+    #FIGVAPER
+    
+    # add model validation! this will largely reflect constraints set in db
+    # V
+    validate :username, :password_digest, :session_token, presence: true
+    validate :username, uniqueness: true
+    validate :password, length: { minimum: 6 }, allow_nil: true
 
+    # A
+    attr_reader :password 
+    after_initialize :ensure_session_token
+
+    # F
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        return null if @user.nil?
+        return @user if @user.is_password?(password)
+    end
+
+    # I
+    def is_password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+
+    # G
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    # E
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    # R
+    def reset_session_token
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
 end
