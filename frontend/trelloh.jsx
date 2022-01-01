1a6cd65a9446193f5935e9a9632a9fec484b965a
Susan Zea <susanzea16@gmail.com>
2021-12-31 20:16:17 -0500

    basic user authentication complete, default state setup

--- frontend/trelloh.jsx
+++ frontend/trelloh.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import {
     signup,
     login,
@@ -9,8 +10,11 @@ import {
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
+    const store = configureStore();
     ReactDOM.render(<h1>Trelloh</h1>, root);
     window.signup = signup;
     window.login = login;
     window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 })
