f5fb884e3b5662f46769d9b0e35a5ada1ea0fe68
Susan Zea <susanzea16@gmail.com>
2022-01-12 01:07:53 -0500

    created list actions file

---
+++ frontend/actions/list_actions.js
@@ -0,0 +1,35 @@
+import { postList, deleteList } from "../util/list_api_util";
+
+
+//action creators
+export const RECEIVE_BOARD_LIST = `RECEIVE_BOARD_LIST`;
+
+export const receiveBoardList = (list) => {
+    return {
+        type: RECEIVE_BOARD_LIST,
+        list
+    }
+}
+
+export const REMOVE_BOARD_LIST = `REMOVE_BOARD_LIST`;
+
+export const removeBoardList = (listId) => {
+    return {
+        type: REMOVE_BOARD_LIST,
+        listId
+    }
+}
+
+
+
+
+//thunk action creators
+export const createList = (list) => (dispatch) => {
+    return postList(list)
+        .then(list => dispatch(receiveBoardList(list)));
+}
+
+export const destroyList = (listId) => (dispatch) => {
+    return deleteList(listId)
+        .then(() => dispatch(removeBoardList(listId)));
+}
