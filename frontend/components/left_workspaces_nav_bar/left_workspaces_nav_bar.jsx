06556281005a63dbfd7e098fdcf5d558e57ee512
Susan Zea <susanzea16@gmail.com>
2022-01-14 02:24:29 -0500

    workspace form toggles

--- frontend/components/left_workspaces_nav_bar/left_workspaces_nav_bar.jsx
+++ frontend/components/left_workspaces_nav_bar/left_workspaces_nav_bar.jsx
@@ -11,6 +11,7 @@ class LeftWorkspacesNavBar extends React.Component {
         
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggle = this.toggle.bind(this);
     }
 
     handleChange(e) {
@@ -23,16 +24,32 @@ class LeftWorkspacesNavBar extends React.Component {
         this.setState({ title: '' })
     }
 
+    toggle(e) {
+        if (e.target.getAttribute('id') === 'show-create-workspace-form') {
+            $(e.target).hide();
+            $(e.target).prev().show();
+        } else {
+            $(e.target.parentElement).hide();
+            $(e.target.parentElement).next().show();
+        }
+    }
 
     render() {
         return(
             <nav className='left-workspaces-nav-bar'>
                 <header id='workspaces-index-header'>
-                    <h3 id="workspaces-index-title">Workspaces</h3>
-                    <form onSubmit={this.handleSubmit}>
-                        <input type="text" onChange={this.handleChange} value={this.state.title} />
-                        <button>+</button>
-                    </form>
+                    <div className="workspaces-index-header-title">
+                        <h3 id="workspaces-index-title">Workspaces</h3>
+                        <form onSubmit={this.handleSubmit} className='create-workspace-form'>
+                            <input type="text" id="workspace-input" 
+                                onChange={this.handleChange} 
+                                value={this.state.title} 
+                                placeholder='Enter a title...'
+                            />
+                            <button id="create-workspace-and-hide-form" onClick={this.toggle}>＋</button>
+                        </form>
+                        <button id="show-create-workspace-form" className='add' onClick={this.toggle}>＋</button>
+                    </div>
                 </header>
                 <WorkspacesIndexContainer />
             </nav>
