b5663644b0e98ea5082072e72e90e1be0729e0a2
Susan Zea <susanzea16@gmail.com>
2022-01-14 01:12:22 -0500

    card index footer toggles

--- frontend/components/cards/card_form.jsx
+++ frontend/components/cards/card_form.jsx
@@ -13,6 +13,9 @@ class CardForm extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggle = this.toggle.bind(this);
+        this.addCardHideForm = this.addCardHideForm.bind(this);
+        this.hideForm = this.hideForm.bind(this);
     }
     
     handleChange(e) {
@@ -25,8 +28,26 @@ class CardForm extends React.Component {
         this.setState({ title: '' })
     }
 
-    addOnClick() {
-        
+    toggle(e) {
+        if (e.target.className === "reveal-card-form") {
+            $(e.target).hide();
+            $(e.target).siblings().show();
+        } else if (e.target.className === "add-card-and-hide-card-form") {
+            $(e.target.parentElement).hide();
+            $(e.target.parentElement).siblings().show();
+        } else if (e.target.className === "hide-card-form") {
+            this.setState({ title: '' })
+            $(e.target.parentElement).hide();
+            $(e.target.parentElement).siblings().show();
+        }
+    }
+
+    addCardHideForm() {
+        return null;
+    }
+
+    hideForm() {
+        return null;
     }
 
 
@@ -35,16 +56,44 @@ class CardForm extends React.Component {
         const toggleInput = document.querySelector('card-form-textarea')
 
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div className='card-form'>
-                    <input type="text" id='card-form-input' placeholder='Enter a title for the card...' value={this.state.title} onChange={this.handleChange} />
-                    <button className="add-button" type='submit'><span className='add'>＋</span> Add a card</button>
-                </div>
-            </form>
+            <div className='card-index-foot'>
+                <button className="reveal-card-form" onClick={this.toggle} type='submit'>
+                    <span className='add'>＋</span> Add a card
+                </button>
+
+
+ 
+                <form className='card-form' style={{display: "none"}} onSubmit={this.handleSubmit}>
+                        <input type="text"
+                            id='card-form-input'
+                            placeholder='Enter a title for the card...'
+                            value={this.state.title}
+                            onChange={this.handleChange}
+                        />
+
+                        <button className="add-card-and-hide-card-form" 
+                            onClick={this.toggle} 
+                            type='submit'
+                        >
+                            Add card
+                        </button>
+
+                        <button className='hide-card-form' onClick={this.toggle} type="button" >✕</button>
+                </form>
+            </div>
         )
     }
 }
 
 export default CardForm;
 
-<button className='form-x-button'>✕</button>
+/* <button className='form-x-button'>✕</button>
+
+
+<form onSubmit={this.handleSubmit}>
+                <div className='card-form'>
+                    <input type="text" id='card-form-input' placeholder='Enter a title for the card...' value={this.state.title} onChange={this.handleChange} />
+                    <button className="add-button" type='submit'><span className='add'>＋</span> Add a card</button>
+                </div>
+            </form> */
+// ^^^ original return
