435cba8118cd78dfd12d04044d91a130d82b6213
Susan Zea <susanzea16@gmail.com>
2022-03-19 11:27:59 -0400

    refactoring CardIndexItem component to be a func instead of class component

--- frontend/components/cards/card_modal_container.jsx
+++ frontend/components/cards/card_modal_container.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function CardModal(props) {
+    // first time using useState for a modal below
+    const [cardModalOpen, openModal] = useState(false)
+
     return (
-        <div>
-            Card Modal
+        <div className='card-modal-background'>
+            <div className='modal-container'>
+                <button onClick={() => openModal(true)}>x</button>
+                <h1>card content</h1>
+                <h2>description</h2>
+                <h2>comments</h2>
+            </div>
         </div>
     )
 }
 
-export default CardModal
+export default CardModal;
