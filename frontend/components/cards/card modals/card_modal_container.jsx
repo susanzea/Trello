c981b8e3c686a752208737b348908e7968bafde1
Susan Zea <susanzea16@gmail.com>
2022-03-21 12:43:28 -0400

    card modal close button styled, card modal positioning fixed

--- frontend/components/cards/card modals/card_modal_container.jsx
+++ frontend/components/cards/card modals/card_modal_container.jsx
@@ -4,7 +4,7 @@ function CardModal(props) {
     return (
         <div className='card-modal-background'>
             <div className='modal-container'>
-                <button id='close-card-modal' onClick={() => props.openModal(false)}>x</button>
+                <button className='close-card-modal' onClick={() => props.openModal(false)}>✕</button>
                 <h1>{props.card.title}</h1>
                 <h2>description</h2>
                 <h2>comments</h2>
--- frontend/components/cards/card_index_item.jsx
+++ frontend/components/cards/card_index_item.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import CardModal from './card_modal_container';
+import CardModal from './card modals/card_modal_container';
 
 function CardIndexItem (props) {
     // first time using useState to conditionally render a modal below
