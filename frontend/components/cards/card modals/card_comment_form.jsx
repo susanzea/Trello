f5a54b65067984905a6c717539e74373c01af011
Susan Zea <susanzea16@gmail.com>
2022-03-22 11:46:48 -0400

    created front end of commenting feature

--- frontend/components/cards/card modals/card_comment_form.jsx
+++ frontend/components/cards/card modals/card_comment_form.jsx
@@ -1,11 +1,78 @@
 import React from "react";
 
 class CardCommentForm extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            id: props.card.id,
+            user_id: props.card.user_id,
+            card_id: props.card.id,
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggle = this.toggle.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ body: e.target.value })
+    }  
+    
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createComment(this.state);
+    }
+
+    toggle(e) {
+        if (e.target.className === "reveal-card-comment-form") {
+            $(e.target).hide();
+            $(e.target).siblings().show();
+        } else if (e.target.className === "save-comment") {
+            $(e.target.parentElement).hide();
+            $(e.target.parentElement).siblings().show();
+        } else if (e.target.className === "hide-card-comment-form") {
+            $(e.target.parentElement).hide();
+            $(e.target.parentElement).siblings().show();
+        }
+    }
 
     render() {
         return (
-            <h1>card Comment form</h1>
+            <div>
+                <br />
+                <br />
+                <h1>Activity</h1>
+                <button className="reveal-card-comment-form" 
+                    onClick={this.toggle} 
+                    type='submit'>
+                    {this.state.body === undefined ? "Add a comment..." : this.state.body}    
+                </button>
+
+                 <form className='card-comment-form' 
+                    onSubmit={this.handleSubmit}
+                    style={{display: "none"}}
+                >
+                        <input type="text"
+                            id='card-comment-form-input'
+                            placeholder="Add a comment..."
+                            value={this.state.body}
+                            onChange={this.handleChange}
+                        />
+
+                        <button className="save-comment" 
+                            type='submit'
+                        >
+                            Save
+                        </button>
+
+                        <button className='hide-card-comment-form' onClick={this.toggle} type="button" >✕</button>
+                </form>
+                <br />
+
+                <br />
+                <br />
+            </div>
         )
     }
 }
