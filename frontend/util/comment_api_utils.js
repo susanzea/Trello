a4f4789821afd358aecf1e523ea88ff8576cec94
Susan Zea <susanzea16@gmail.com>
2022-03-22 17:14:28 -0400

    comment api utils added

---
+++ frontend/util/comment_api_utils.js
@@ -0,0 +1,37 @@
+export const getAllCardComments = (cardId) => {
+    return $.ajax({
+        method: 'GET',
+        url: `/api/cards/${cardId}/comments`
+    })
+}
+
+export const getCardComment = (commentId) => {
+    return $.ajax({
+        method: 'GET',
+        url: `/api/comments/${commentId}`
+    })
+}
+
+export const postComment = (comment) => {
+    return $.ajax({
+        method: 'POST',
+        url: `/api/comments`,
+        data: { comment }
+    })
+}
+
+export const deleteComment = (commentId) => {
+    return $.ajax({
+        method: 'DELETE',
+        url: `/api/comments/${commentId}`,
+        data: { commentId }
+    })
+}
+
+export const patchComment = (comment) => {
+    return $.ajax({
+        method: 'PATCH',
+        url: `/api/comments/${comment.id}`,
+        data: { comment }
+    })
+}
