c32c01340d4d94b83b4c6130439e7e0db777760b
Susan Zea <susanzea16@gmail.com>
2022-03-22 16:44:20 -0400

    comments controller added

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,47 @@
+class Api::CommentsController < ApplicationController
+    
+    def index
+        @comments = Comment.all.where(card_id: params[:card_id])
+        render :index
+    end 
+
+    def show
+        @comment = Comment.find(params[:id])
+        render :show
+    end
+
+    def create
+        @comment = Comment.new(new_comment_params)
+
+        if @comment.save
+            render 'api/comments/show'
+        else
+            render json: @card.errors.full_messages, status: 422
+        end 
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+
+        if @comment
+            @comment.destroy
+        else
+            render json: ['That comment does not exist'], status: 404
+        end
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        
+        if @comment.update(new_comment_params)
+            render :show
+        else
+            render json: @comment.errors.full_messages, status: 422
+        end
+    end
+
+
+    def new_comment_params
+        params.require(:comment).permit(:id, :body, :user_id, :card_id)
+    end
+end
