91f2b9e8b3c88f2b13ca807223caa631d2dd926b
Susan Zea <susanzea16@gmail.com>
2022-04-05 19:38:39 -0400

    finished styling create workspace modal

--- frontend/components/left_workspaces_nav_bar/create_workspace/create_workspace_form.jsx
+++ frontend/components/left_workspaces_nav_bar/create_workspace/create_workspace_form.jsx
@@ -25,16 +25,19 @@ class CreateWorkspaceForm extends React.Component {
 
 
     render() {
+
         return (
             <form className='create-workspace-form'
                 onSubmit={this.handleSubmit}    
             >
+                <h3>Workspace name</h3>
                 <input type="text" id="workspace-input" 
                     onChange={this.handleChange} 
                     value={this.state.title} 
-                    placeholder='Enter a title...'
+                    placeholder='Recruitment Team'
                 />
-                <button type='submit'>Create</button>
+                <p>This is the name of your company, team or organization.</p>
+                 <button id={this.state.title ? 'title-present' : 'no-title'}  type={this.state.title ? 'submit' : 'button'} >Create</button>
             </form>
         )
     }
--- frontend/components/left_workspaces_nav_bar/create_workspace/create_workspace_modal.jsx
+++ frontend/components/left_workspaces_nav_bar/create_workspace/create_workspace_modal.jsx
@@ -9,10 +9,14 @@ function CreateWorkspaceModal(props) {
                 <button className='close-create-workspace-modal' 
                     onClick={() => props.openCreateWorkspaceModal(false)}
                 >✕</button>
-                <CreateWorkspaceForm 
-                    openCreateWorkspaceModal={props.openCreateWorkspaceModal}
-                />
-                {/* <img src={window.workspace_modal_img} alt="image of a trelloh board" className='workspace-modal-img' /> */}
+                <img src={window.workspace_modal_img} alt="image of a trelloh board" className='workspace-modal-img' />
+                <div id='main-content'>
+                    <h1>Let's create a Workspace</h1>
+                    <h2>Boost your productivity by making it easier for everyone to access tasks in one location.</h2>
+                    <CreateWorkspaceForm 
+                        openCreateWorkspaceModal={props.openCreateWorkspaceModal}
+                    />
+                </div>
             </div>
         </div>
     )
