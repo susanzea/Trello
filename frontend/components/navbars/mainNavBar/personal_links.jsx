1493f87a6d0f8f7ff73674077b260f058ab600aa
Susan Zea <susanzea16@gmail.com>
2022-04-20 13:37:59 -0400

    links now open in new tab

--- frontend/components/navbars/mainNavBar/personal_links.jsx
+++ frontend/components/navbars/mainNavBar/personal_links.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const personalLinks = (props) => {
     return(
         <div id="personal-links">
-            <a href="https://angel.co/u/susan-zea">
+            <a href="https://angel.co/u/susan-zea" target='_blank'>
                 <img id="angellist" src={window.angellist} alt="angellist link" />
             </a>
-            <a href="https://github.com/susanzea">
+            <a href="https://github.com/susanzea" target='_blank'>
                 <img id="github" src={window.github} alt="github link" />
             </a>
-            <a href="https://github.com/susanzea/trelloh">
+            <a href="https://github.com/susanzea/trelloh" target='_blank'>
                 <img id="source" src={window.source} alt="portfolio link" />
             </a>
         </div>
